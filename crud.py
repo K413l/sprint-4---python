5f35f5e29f29c6a87dc83bd480dc3bb525dc91ae
K413l <kalel.schlichting@gmail.com>
2023-11-13 23:22:04 -0300

    commit final codigo finalizado e pronto

--- crud.py
+++ crud.py
@@ -2,16 +2,20 @@ import oracledb
 import csv
 
 connection = oracledb.connect(user = 'RM551007',password = '030803',dsn = 'oracle.fiap.com.br/orcl') 
+#connection = oracledb.connect(user = 'RM550620',password = '130400',dsn = 'oracle.fiap.com.br/orcl') 
 cursor = connection.cursor()
 
 def listar_colunas(tabelaS):
     cursor = connection.cursor()
-
     cursor.execute(f"SELECT column_name FROM user_tab_columns WHERE table_name = '{tabelaS.upper()}'")
 
-    print(f"Colunas da tabela {tabelaS}:")
-    for coluna in cursor:
-        print(coluna[0])
+    colunas = [coluna[0] for coluna in cursor.fetchall()]  # Corrigido aqui
+    print(f"Colunas da tabela {tabelaS}: {', '.join(colunas)}")  # Ajustado aqui
+
+    cursor.close()
+
+    return colunas  # Adicionado o retorno das colunas
+
 
 
 def listar_tabelas():
@@ -40,7 +44,7 @@ def inserir_dados():
 
     # Obter os valores a serem inseridos dinamicamente
     valores = []
-    for coluna in listar_colunas(connection, tabelaS):
+    for coluna in listar_colunas(tabelaS):
         valor = input(f"Insira um valor para a coluna {coluna}: ")
         valores.append(valor)
 
@@ -57,14 +61,21 @@ def inserir_dados():
 def atualizar_dados():
     tabelaS = input("Qual tabela deseja atualizar um registro: ")
     listar_colunas(tabelaS)
-    id = int(input("Digite o ID do registro que você quer atualizar: "))
     coluna = input("Qual coluna deseja atualizar o valor")
+    colunaID = input("Qual coluna ID da tabela")
+    id = int(input("Digite o ID do registro que você quer atualizar: "))
     valor = input("Digite o novo valor")
-    cursor.execute(f"UPDATE {tabelaS} SET {coluna} = :1 WHERE id = :2", (valor, id))
+    cursor.execute(f"UPDATE {tabelaS} SET {coluna} = :1 WHERE {colunaID} = :2", (valor, id))
     connection.commit()
 
 
-def excluir_dados(dados):
-    tabelaS = input("Qual tabela deseja deletar um registro: ")
-    cursor.execute("DELETE FROM funcionarios WHERE id = :1", (id,))
+def excluir_dados():
+    tabelaS = str(input("Qual tabela deseja deletar um registro: "))
+    listar_colunas(tabelaS)
+    Coluna = input("Coluna que deseja selecionar: ")
+    id_para_deletar = input("Qual é o ID do registro que deseja excluir: ")
+    
+    
+    cursor = connection.cursor()
+    cursor.execute(f"DELETE FROM {tabelaS} WHERE {Coluna} = :1", (id_para_deletar,))
     connection.commit()
